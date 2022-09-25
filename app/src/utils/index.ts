33e36deb365e069e7bb59c6fd0c5bf576098b4d3
Gabriel Larue <gabriellarue.dev@gmail.com>
2022-09-25 10:07:59 -0400

    Fix checkFileContent utils func

--- app/src/utils/index.ts
+++ app/src/utils/index.ts
@@ -1,24 +1,32 @@
-const checkFileHeader = (lines: string[]): string[] => {
-    for (var i = 1; i < lines.length - 1; i++) {
-        if(lines[i].length !== lines[i-1].length){
-            lines.shift()
+const checkFileLines = (lines: string[]): string[] => {
+    var spliceToIndex = -1;
+
+    for (var i = 1; i < lines.length; i++) {
+        var previous = i - 1
+
+        if (lines[i].split(',').length !== lines[previous].split(',').length) {
+            spliceToIndex = previous
         }
     }
-    
+
+    if (spliceToIndex !== -1) {
+        lines.splice(0, spliceToIndex + 1)
+    }
+
     return lines
 }
 
 export const checkFileContent = (file: File): Promise<File> => {
     const reader = new FileReader()
 
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         reader.readAsText(file)
 
         reader.onload = () => {
             if (typeof reader.result === 'string') {
                 var lines = reader.result.split('\n')
 
-                lines = checkFileHeader(lines)
+                lines = checkFileLines(lines)
 
                 var cleanedFile = new File(lines, file.name, { type: file.type, lastModified: Date.now() })
 
